8a96300a7018caab50b36561d67bda2aa917f2aa
Dmitriy Pischik <pischik@bk.ru>
2023-01-22 17:13:09 +1000

    Added folder Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    sum = sum + i;
+    // Console.WriteLine(i + " " + sum);
+}
+Console.WriteLine("Сумма чисел от 1 до " + n + " равна " + sum);
