98cbecb11bfc741192575efdef81d7d37f0e19ba
Dmitriy Pischik <pischik@bk.ru>
2023-01-22 20:42:23 +1000

    Added folder Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] new_array() // функция задаёт массив из 8 элементов, заполненных нулями и единицами.
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 2); 
+    }
+    return array;
+}
+
+int[] arr = new_array();
+Console.WriteLine("Выведем на экран сгенерированный массив:");
+foreach (int el in arr) 
+{
+    Console.Write(el + " ");
+}
