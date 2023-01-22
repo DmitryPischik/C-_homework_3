ab4c8d7aaff0669b2bc7d45c63986911d90bb10b
Dmitriy Pischik <pischik@bk.ru>
2023-01-22 19:01:50 +1000

    Added folder Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int product = 1;
+for (int i = 1; i <= n; i++)
+{
+    product = product * i;
+    //Console.WriteLine(i + " " + product);
+}
+Console.WriteLine("произведения чисел от 1 до " + n + " равно " + product);
