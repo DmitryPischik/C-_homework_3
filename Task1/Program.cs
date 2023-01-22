4d9e925ecfea8744cd5d98ccb3451b2e89bcd224
Dmitriy Pischik <pischik@bk.ru>
2023-01-22 17:07:52 +1000

    Added folder Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Чтобы получить кубы чисел от 1 до N ведите число N : ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(i + " " + i * i * i);
+}
