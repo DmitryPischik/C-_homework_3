109fdbe5e0f4278daf181c972ddd75ea5a631e98
Dmitriy Pischik <pischik@bk.ru>
2023-01-22 18:39:47 +1000

    Added folder Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите натуральное число В: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+int result = 1;
+for (int i = 1; i <= B; i++)
+{
+    result = A * result;
+}
+Console.WriteLine(A + " в степени " + B + " равно " + result);
