d45f73d225514d09ff9aec6c22cdbabe26d93785
Dmitriy Pischik <pischik@bk.ru>
2023-01-22 18:45:39 +1000

    Added folder Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите целое число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int count = 0;
+
+while (n != 0)
+{
+    n = n / 10;
+    count++;
+}
+Console.WriteLine("Введённом числе " + count + " цифр");
