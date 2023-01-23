11653da5fcd8b614163198660d5ae8f2a855af39
Dmitriy Pischik <pischik@bk.ru>
2023-01-23 20:41:20 +1000

    Added folder Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,34 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] new_array() // функция задаёт массив из 123 целых чисел.
+{
+    int[] array = new int[123];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 300); 
+    }
+    return array;
+}
+void PrintArr(int[] array) // функция просматривает массив целых часел.
+{  
+    foreach (int item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+void CheckTwoArr(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if ((item >10 && item < 99)) {count++;}
+    }
+    Console.WriteLine(count);
+}
+////////////////////////////
+
+int[] arr = new_array();
+PrintArr(arr);
+CheckTwoArr(arr);
