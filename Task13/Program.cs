9d7c0d50af6103b50627992f837990403ed34b2f
Dmitriy Pischik <pischik@bk.ru>
2023-01-23 21:38:15 +1000

    Added folder Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,41 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] new_array(int number) // функция задаёт массив с заданным кол-м случайных вещественных чисел.
+{
+    double[] array = new double[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * new Random().Next(1, 10); 
+    }
+    return array;
+}
+void PrintArr(double[] array) // функция просматривает массив вещественных часел.
+{  
+    foreach (double item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+void diminutionMaxAndMin(double[] array) // функция находит разницу между максимальным и минимальным элементом в массиве вещественных чисел.
+{
+    double min = 10000;
+    double max = 0;
+    double result = 0;
+    foreach (double item in array)
+    {
+        if (item < min) min = item;
+        if (item > max) max = item;
+    } 
+    result = max - min;
+    Console.WriteLine($"Разницу между максимальным и минимальным элементом = {result}");  
+}
+//////////////
+
+Console.Write("Укажите число элементов для нового массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+double[] arr = new_array(n);
+Console.WriteLine("Посмотрим сгенерированный массив:");
+PrintArr(arr);
+diminutionMaxAndMin(arr);
